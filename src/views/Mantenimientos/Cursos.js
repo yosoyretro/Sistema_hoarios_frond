22291bbd9730fd8a12ed762afc053d8878899329
KevinNavarr <102337594+KevinNavarr@users.noreply.github.com>
2024-01-14 10:16:30 -0500

    a

--- src/views/Mantenimientos/Cursos.js
+++ src/views/Mantenimientos/Cursos.js
@@ -2,6 +2,7 @@ import React,{useState, useEffect, useRef, Children} from "react";
 import { Button, Col, Row,Form, Flex, Breadcrumb, Table, Popconfirm,Spin,Space,Modal,Input,notification } from "antd";
 
 import { ToolOutlined, QuestionCircleOutlined, DeleteOutlined, EditOutlined, SyncOutlined,SaveOutlined,FileAddOutlined } from '@ant-design/icons';
+import { error } from "console";
 
 const Nivel = () => {
     const [loading,setLoading]= useState(true);
@@ -64,8 +65,87 @@ const Nivel = () => {
                         setDataNivel(obj)
                         
                     }
+                } else if (data.ok ==false) {
+                    mostrarNotificacion("error", "A ocurrido un error", data.msg_error);
+                } else {
+                    throw new Error("El error es interno en el servidor, por faovr contactese con el administrador.");
                 }
+
+            }).finally(()=>{
+                setLoading(false);
+            }).catch((error) => {
+                mostrarNotificacion('error', 'A ocurrido un error',error.message)
             })
+
+    }
+
+    const createNivel = (value) =>{
+        let data_request = {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json', //Especifica el tipo de contenido como JSON
+            },
+            body: JSON.stringify(value),
+        }
+        fetch(`${url}create_nivel/`,data_request).then((response)=>{return response.json()})
+        .then((request_backend)=>{
+            if(request_backend.ok){
+                mostrarNotificacion("success", "Operación realizada con éxito", request_backend.msg);
+            } else if(request_backend.ok == false) {
+                mostrarNotificacion("error", "Hubo un problema", request_backend.msg);
+            } else{
+                throw new Error("El error es interno en el servidor, por favor contactarse con el administrador.");
+            }
+        }).finally(
+            ()=>{
+                setModalOpen(false);
+                form.resetFields();
+                getNivel();
+            }
+        ).catch((error)=> {
+            mostrarNotificacion("error", "A ocurrido un error",error);
+        })
+    }
+
+    const deleteNivel = (values) => {
+        let request_backend = {
+            method:"POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body:JSON.stringify({
+                id_nivel:values.id_nivel
+            })
+        }
+        fetch(`${url}delete_nivel/`,request_backend).then((data_request)=>{return data_request.json()})
+        .then((data)=>{
+            if (data.ok){
+                mostrarNotificacion("success","Operación realizada con éxito","El titulo" + values.descripcion.props.nivel + "se eliminó con éxito");
+            }else if(data.ok == false){
+                mostrarNotificacion("error","A ocurrido un error interno", data.msg);
+            }
+        }).finally(()=>{
+            getNivel();
+        })
+    }
+    useEffect(()=>{
+        getNivel();
+    },[])
+
+    function onFinish(values){
+        createNivel(values)
+    }
+
+    function editNivel(values){
+        let request_backend = {
+            method:"POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body:JSON.stringify({
+                id_nivel:
+            })
+        }
     }
 
 }
