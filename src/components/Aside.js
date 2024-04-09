fe5f7de5918d92b5e85bf2ba55e9ae00e19ded17
jordzk <149849603+jordzk@users.noreply.github.com>
2024-04-09 00:11:12 -0500

    actualización de iconos del aside

--- src/components/Aside.js
+++ src/components/Aside.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import { Sidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
-import { SettingOutlined, UserOutlined, BankOutlined } from "@ant-design/icons";
+import { SettingOutlined, UserOutlined, BankOutlined, CalendarOutlined, BookOutlined, GlobalOutlined, FolderOutlined, ApartmentOutlined, UserSwitchOutlined } from "@ant-design/icons";
 import { Link } from "react-router-dom"
 import { Button } from "antd";
 
@@ -46,31 +46,31 @@ const Aside = ({ children }) => {
                 </Link>
 
                 <Link to={"Mantenimientos/perfiles"}>
-                  <MenuItem disabled icon={<UserOutlined />} style={{ background: '#000000', color: 'white' }}>{"Perfiles"}</MenuItem>
+                  <MenuItem disabled icon={<UserSwitchOutlined />} style={{ background: '#000000', color: 'white' }}>{"Perfiles"}</MenuItem>
                 </Link>
 
                 <Link to={"Mantenimientos/cursos"}>
-                  <MenuItem disabled icon={<SettingOutlined />} style={{ background: '#000000', color: 'white' }}>{"Cursos"}</MenuItem>
+                  <MenuItem disabled icon={<ApartmentOutlined />} style={{ background: '#000000', color: 'white' }}>{"Cursos"}</MenuItem>
                 </Link>
 
                 <Link to={"Mantenimientos/paralelos"}>
-                  <MenuItem disabled icon={<SettingOutlined />} style={{ background: '#000000', color: 'white' }}>{"Paralelos"}</MenuItem>
+                  <MenuItem disabled icon={<ApartmentOutlined />} style={{ background: '#000000', color: 'white' }}>{"Paralelos"}</MenuItem>
                 </Link>
 
                 <Link to={"Mantenimientos/educacionGobal"}>
-                  <MenuItem disabled icon={<BankOutlined />} style={{ background: '#000000', color: 'white' }}>{"Educacion Global"}</MenuItem>
+                  <MenuItem disabled icon={<GlobalOutlined />} style={{ background: '#000000', color: 'white' }}>{"Educacion Global"}</MenuItem>
                 </Link>
 
                 <Link to={"Mantenimientos/materias"}>
-                  <MenuItem disabled icon={<BankOutlined />} style={{ background: '#000000', color: 'white' }}>{"Materias"}</MenuItem>
+                  <MenuItem disabled icon={<BookOutlined />} style={{ background: '#000000', color: 'white' }}>{"Materias"}</MenuItem>
                 </Link>
 
                 <Link to={"Mantenimientos/tituloacademico"}>
-                  <MenuItem disabled icon={<BankOutlined />} style={{ background: '#000000', color: 'white' }}>{"Titutlos Academicos"}</MenuItem>
+                  <MenuItem disabled icon={<FolderOutlined />} style={{ background: '#000000', color: 'white' }}>{"Titutlos Academicos"}</MenuItem>
                 </Link>
 
                 <Link to={"Mantenimientos/horarios"}>
-                  <MenuItem disabled icon={<BankOutlined />} style={{ background: '#000000', color: 'white' }}>{"Horarios"}</MenuItem>
+                  <MenuItem disabled icon={<CalendarOutlined />} style={{ background: '#000000', color: 'white' }}>{"Horarios"}</MenuItem>
                 </Link>
 
               </SubMenu>
